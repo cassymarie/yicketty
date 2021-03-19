ef4bafb1b9b21c4a3e12c51f5582bc885e9e5a7d
cassymarie <cassy2205@gmail.com>
2021-03-19 00:00:44 -0600

    minor tweaks to autologin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Switch, Route } from 'react-router-dom'
 import { getMlbTeams } from './redux/MlbActionCreators.js'
+import { autoLogin } from './redux/UserActionCreators.js'
 import MlbTeams from './containers/MlbTeams.js'
 import TeamPage from './components/mlb/TeamPage.js'
 // import NavBar from './containers/NavBar.js'
@@ -10,15 +11,18 @@ import Login from './components/login/Login.js'
 class App extends Component {
 
   componentDidMount(){
+    
+    localStorage.token && this.props.autoLogin()
     this.props.getMlbTeams()
   }
 
   render() {
-    const color = this.props.mlbTeam.id ? this.props.mlbTeam.color1 : '#e1e7eb'
+    const color = this.props.mlbTeam.id ? this.props.mlbTeam.color1 : '#fff'
     return (
           <div className="app container-fluid" style={{backgroundColor:`${color}`}}>
             {this.props.user.id ? 
               <Switch>
+                <Route path="/" component={MlbTeams}/>
                 <Route path="/mlbteams/:id" component={TeamPage}/>
                 <Route path="/mlbteams" component={MlbTeams}/>
               </Switch>
@@ -36,4 +40,4 @@ const mapStateToProps = (state) => ({
 })
 
 
-export default connect(mapStateToProps,{ getMlbTeams })(App);
+export default connect(mapStateToProps,{ getMlbTeams, autoLogin })(App);
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,35 +1,54 @@
 import React from 'react';
-import { addNewUser, toggleSignup } from "../../redux/UserActionCreators.js"
 import { connect } from 'react-redux';
+import { toggleSignup, handleLoginFormChange, sendSignup, sendLogin } from "../../redux/UserActionCreators.js"
 
 const Login = (props) => {
 
-  const { signup, toggleSignup } = props
+  const { form, handleLoginFormChange, sendLogin, sendSignup, signup, toggleSignup } = props
+  const { name_first, name_last, password, passwordConfirmation, username } = form
+  const onSubmit = (event) => { 
+        event.preventDefault()
+        if (signup){
+          if (password === passwordConfirmation){
+            sendSignup({username: username, name_first: name_first, name_last: name_last, password: password})
+          } else {
+            alert("Those passwords do not match")
+          }
+        } else {
+          sendLogin({username: username, password: password})
+        }
+     }
 
   return(
-    <>
-      <h3>{ signup ? 'Sign up' : 'Login'}</h3>
-      <form>
-        { signup && 
+    <div className="login">
+      <h3>{ signup ? "Sign Up" : "Login"}</h3>
+      <>
+      <form onSubmit={ onSubmit }>
+      { signup ?
         <>
-          <label>Name:</label>
-          <input type="text" name="first_name" placeholder="first"/>
-          <input type="text" name="last_name" placeholder="last"/><br/>
-        </>
-        }
-        <label>Username: <input type="text" name="username"/></label><br/>
-        <label>Password: <input type="password" name="password"/></label>
-        { signup && <><input type="password" name="passwordConfirmation" placeholder="confirm password"/></>}
-        <br/>
-        <input type="submit" value="Submit" />
+          <input type="text" name="name_first" value={name_first} onChange={handleLoginFormChange} placeholder="First name"/><br/>
+          <input type="text" name="name_last" value={name_last} onChange={handleLoginFormChange} placeholder="Last name" /><br/>
+        </> 
+        : <></>}
+          <input type="text" name="username" value={username} onChange={handleLoginFormChange} placeholder="username" /><br/>
+          <input type="password" name="password" value={password} onChange={handleLoginFormChange} placeholder="password" /><br/>
+      { signup ? 
+        <>
+          <input type="password" name="passwordConfirmation" value={passwordConfirmation} onChange={handleLoginFormChange} placeholder="Confirm password" /><br/>
+        </> : <></>}
+        <input type="submit" value="Enter" /><br/>
       </form>
+      </>
       <br/>
-      <br/>
-      <button onClick={toggleSignup}> { signup ? 'Login':'Sign up'}</button>
-    </>
+      <button onClick={toggleSignup}>{ signup ? "Login" : "Sign up"}</button>
+    </div>
   )
+
 }
 
-const mapStateToProps = (state) => ({ ...state.user })
+const mapStateToProps = (state) => ({ 
+  signup: state.user.signup,
+  form: state.user.loginForm
+ })
 
-export default connect(mapStateToProps, { addNewUser, toggleSignup })(Login)
+export default connect(mapStateToProps, { toggleSignup, handleLoginFormChange, sendSignup, sendLogin })(Login)
