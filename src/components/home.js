7f8d07080addaacd8c0b55ae4e9501b546f24595
cassymarie <cassy2205@gmail.com>
2021-03-29 16:06:45 -0600

    updates to home page and background imgs

--- src/components/home.js
+++ src/components/home.js
@@ -5,8 +5,11 @@ const Home = (props) => {
   return (
     <>
     <div className="home-page">
-      <h2 className="home-page-title"> Home Page </h2>
-        <p  className="home-page-description"> this is the homepage I should write something Fabulous about baseball here!</p>
+      <h2 className="home-page-title"> Yicketty! </h2>
+        <p  className="home-page-description"> Play Yicketty, a fun spin on America's favorite past time!</p>
+        <p  className=""> Eventually</p>
+        <p  className="home-page-description wider">At this site, you can view the current team roster for your favorite MLB Team.<br></br>Interested in a player, view their career stats.</p>
+        <p  className="home-page-description"> Start to put together a lineup to play a game.  </p>
     </div>
     </>
   )
--- src/containers/MlbTeams.js
+++ src/containers/MlbTeams.js
@@ -15,39 +15,39 @@ class MlbTeams extends Component {
     const westDiv = leagueTeams.filter(team => team.division === "W")
     return (
       <Container className="divisions">
-        <h2>East Division</h2>
+        <h2 className="division-title">East Division</h2>
         <Row className="division">
           {eastDiv.map(team => <TeamCard key={team.id} {...team}/>)}
         </Row>
-        <h2>Central Division</h2>
+        <h2 className="division-title">Central Division</h2>
         <Row className="division">
           {centralDiv.map(team => <TeamCard key={team.id} {...team}/>)}
         </Row>
-        <h2>West Division</h2>
+        <h2 className="division-title">West Division</h2>
         <Row className="division">
           {westDiv.map(team => <TeamCard key={team.id} {...team}/>)}
         </Row>
       </Container>
-    )
+    ) 
   }
 
   render(){
     return(
-      <>
+      <div className="mlb-team-page">
       <Row>
         <>
         <Col className="league">
-          <h1>American Leaguge</h1>
+          <h1 className="league-title">American Leaguge</h1>
           {this.teamsByLeague("AL")}
         </Col>
 
         <Col className="league">
-          <h1>National Leaguge</h1>
+          <h1  className="league-title">National Leaguge</h1>
           {this.teamsByLeague("NL")}
         </Col>
         </>      
       </Row>
-      </>
+      </div>
     )
   }
     
